04e80b156ca2f910be16f77000c30cef20c0f7bb
Alekseev Oleg <01.wftp@gmail.com>
2023-04-13 12:13:55 +0300

    Initial Commit

---
+++ MyGallary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyGallary
+//
+//  Created by Олег Алексеев on 12.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
